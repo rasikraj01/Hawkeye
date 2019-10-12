2f4799c25560c2dfffcb7783f2c7b0e0cba81d48
Jahnavi Seth <jahnavi_1999@live.com>
2019-10-13 00:02:14 +0530

    hello world react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './css/app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello world !!
+      </div>  
+    );
+  }
 }
 
 export default App;
